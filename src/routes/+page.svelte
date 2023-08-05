51368cd6b9763034cfb97a6bba2dafc8f773f655
Hyun-Tae Jin <htjmario@gmail.com>
2023-08-04 22:59:28 -0400

    added dark bg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Hadrian's Wall</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="bg-slate-800 w-screen h-screen">
+	<h1>Hadrian's Wall</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
